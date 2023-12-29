5532306ed11fb83573b67c3612b70b5c1d059925
Replit user <>
2023-12-29 05:00:16 +0000

    Everything

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer');
+const upload = multer()
+const mongoose = require('mongoose')
+const { Schema } = mongoose
 require('dotenv').config()
+
+mongoose.connect(process.env.DB_URL)
+
+const FileDetailsSchema = new Schema({
+  name: String,
+  type: String,
+  size: Number
+})
+
+const FileDetails = mongoose.model("File Details", FileDetailsSchema);
 
 var app = express();
 
@@ -11,8 +25,21 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', upload.single('upfile'), async (req, res) => {
+  console.log(req)
+  const fileObject = new FileDetails({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+  try {
+    const file = await fileObject.save()
+    console.log(file)
+    res.json (fileObject)
+  } catch (err) {
+      console.log(err)
+  }
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
